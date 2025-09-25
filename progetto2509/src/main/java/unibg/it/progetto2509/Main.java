8520c4320e21ab46b4d66d2070f6c5f2e67e69dc
Luizo <101356110+CuniLuizo@users.noreply.github.com>
2025-09-25 16:56:43 +0200

    Aggiornamento classe Main

--- progetto2509/src/main/java/unibg/it/progetto2509/Main.java
+++ progetto2509/src/main/java/unibg/it/progetto2509/Main.java
@@ -1,5 +1,28 @@
 package unibg.it.progetto2509;
 
-public abstract class Main {
+import java.util.Scanner;
 
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Inserisci il primo valore: ");
+		int primo = sc.nextInt();
+		
+		System.out.print("Inserisci il secondo valore: ");
+		int secondo = sc.nextInt();
+		
+		Calculator c = new Calculator(primo, secondo);
+		
+		c.somm(primo, secondo);
+		c.diff(primo, secondo);
+		c.prod(primo, secondo);
+		c.div(primo, secondo);
+		
+		sc.close();
+		
+	}
+	
 }
